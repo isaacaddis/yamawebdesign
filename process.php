08edc02a98bcdfd88caa3496f2664b4dedef6b95
Isaac Addis <isaacaddis21@gmail.com>
2017-11-13 14:59:43 -0800

    Its lit!

--- process.php
+++ process.php
@@ -4,4 +4,7 @@
 	$message = $_POST['message'];
 	//to, subject, message, headers
 	mail("isaacaddis21@gmail.com",$name,$message,"Yama Web Design Email");
+	echo("<script>
+             window.location=\"index.html\";
+     </script>");
 ?>
